9c4a95de1e5e8100e9d023e9ae1179af0a483ffc
karishma2gupta <138330411+karishma2gupta@users.noreply.github.com>
2024-02-05 11:12:13 +0530

    Add files via upload

---
+++ src/Data/QuizData.js
@@ -0,0 +1,53 @@
+export const QuizData = [
+    {
+        question: " Which of the following is not a type of Constructor in C++?",
+        options:[ "Default constructor ", "Parameterized constructor" , "Copy constructor " , "Friend constructor" ],
+        answer:4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: " Which of the following user-defined header file extension used in c++ ?",
+        options: ["hp", "cpp", "h", "hf"],
+        answer:3
+    },
+    {
+        question: "Which of the following approach is used by C++?",
+        options: ["Left-right" ," Right-left" , "Bottom-up" ,"Top-down"],
+         answer:3
+    },
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+       
+    },
+    {
+        question: " Who invented c++ ?",
+        options: [" Dennis Ritchie " , "Brian Kernighan" , "Bjarne Stroustrup"  , " Ken Thompson"],
+        answer:4
+    },
+    {
+        question: "  Which is more effective while calling the C++ functions  ?",
+        options: ["  call by object " , "  call by pointer" , "call by refrence"  , "  call by value"],
+        answer:3
+    },
+    {
+        question: " Which of the following is used to terminate the function declaration in C++  ?",
+        options: ["  ;" , " :" , " ]"  , ")"],
+        answer:1
+    },
+];
